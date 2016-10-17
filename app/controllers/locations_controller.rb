7e3f8616c816862632a702782d845d6824dccca5
Julie Graceffa <julie.graceffa@gmail.com>
2016-10-17 15:17:51 -0400

    changed search bar form to be a get request and save input as a new location

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,9 +2,28 @@ class LocationsController < ApplicationController
 
   def index
     @locations = Location.all.sort_by { |l| l.city }
+    @location = Location.new
   end
 
   def show
     @location = Location.find(params[:id])
+  end
+
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+
+    if @location.save
+      redirect_to location_categories_path(@location)
+    else
+      render :index
+    end
+  end
+
+  def location_params
+    params.require(:location).permit(:city)
   end
 end
