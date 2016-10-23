7a4b7a9a51f3861a0f087822e1657c7054748476
Julie Graceffa <julie.graceffa@gmail.com>
2016-10-22 21:30:48 -0400

    added static google map on workout show page with javascrip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021173816) do
+ActiveRecord::Schema.define(version: 20161023011112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,8 @@ ActiveRecord::Schema.define(version: 20161021173816) do
     t.string   "phone"
     t.integer  "rating"
     t.string   "street"
+    t.string   "longitude"
+    t.string   "latitude"
   end
 
 end
